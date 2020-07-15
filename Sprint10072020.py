eb85bd95ea7a36e30caa14073d5a90baf4b5fc79
therealdavidbrown <64203103+therealdavidbrown@users.noreply.github.com>
2020-07-16 01:52:52 +0200

    Add files via upload

---
+++ Sprint10072020.py
@@ -0,0 +1,38 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="putain",
+    database="testdb"
+    )
+cursor = mydb.cursor()
+#cursor.execute("CREATE DATABASE products")
+cursor.execute("SHOW DATABASES")
+
+#for db in cursor:
+    #print(db)
+
+#cursor.execute("CREATE TABLE products(name VARCHAR(255), category VARCHAR(255))")
+#cursor.execute("SHOW TABLES")
+for tb in cursor:
+    print(tb)
+sqlFormula = "INSERT INTO products (name, category) VALUES (%s, %s)"
+items = [   ("Simba", "Chips"),
+            ("Lays", "Chips"),
+            ("Coke","Cooldrink"),
+            ("Fanta","Cooldrink"),
+            ("Cadbury","Chocolates"),
+            ("Tex","Chocolates"),
+            ("Pepper Steak","Pies"),
+            ("Chicken","Pies"),
+            ("Pear","Fruit"),
+            ("Apple","Fruit"),
+            ("Orange","Fruit"),
+            ("Vanilla","Cupcakes"),
+            ("Chocolate","Cupcakes"),
+            ("Spinach","Veggies"),
+            ("Cabbage","Veggies"),]
+
+cursor.executemany(sqlFormula, items)
+mydb.commit
