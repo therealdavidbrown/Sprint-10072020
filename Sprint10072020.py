479cc0d8b7917443087878a88d73450e9a14aa35
therealdavidbrown <64203103+therealdavidbrown@users.noreply.github.com>
2020-07-16 02:05:34 +0200

    Add files via upload

--- Sprint10072020.py
+++ Sprint10072020.py
@@ -4,17 +4,17 @@ mydb = mysql.connector.connect(
     host="localhost",
     user="root",
     passwd="putain",
-    database="testdb"
+    database="products"
     )
 cursor = mydb.cursor()
 #cursor.execute("CREATE DATABASE products")
-cursor.execute("SHOW DATABASES")
+#cursor.execute("SHOW DATABASES")
 
 #for db in cursor:
     #print(db)
 
 #cursor.execute("CREATE TABLE products(name VARCHAR(255), category VARCHAR(255))")
-#cursor.execute("SHOW TABLES")
+cursor.execute("SHOW TABLES")
 for tb in cursor:
     print(tb)
 sqlFormula = "INSERT INTO products (name, category) VALUES (%s, %s)"
